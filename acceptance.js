b1e4c7051087140a386029bf703f18654bda4cf4
kdali9 <74625156+kdali9@users.noreply.github.com>
2023-08-08 20:14:42 +0300

    Update the automated acceptance.js file.

--- acceptance.js
+++ acceptance.js
@@ -1,27 +1,67 @@
 const puppeteer = require('puppeteer');
+
 const assert = require('assert');
 
+
 try{
-    (async () => {
-    const browser = await puppeteer.launch({headless:true});
-    const page = await browser.newPage();
-    await page.goto("https://usiu-facility-booking.onrender.com");
 
-    const searchInput = await page.$('#capacity')
-    await searchInput.type('22' )
-   
-    const searchBtn = await page.$('#book-event-btn')
-    await searchBtn.click()
-   
-    const pageTitle = await page.title();
-   
-    assert(pageTitle === 'Report');
-    console.log("Title matched successfully");
+    (async () => {
 
-    await browser.close();
-    })();
+    const browser = await puppeteer.launch({headless:true});
+
+    const page = await browser.newPage();
+
+    await page.goto("https://usiu-facility-booking.onrender.com/");
+
+
+    const venue = await page.$('#venue')
+
+    await venue.type('auditorium' )
+
+
+    const name = await page.$('#name')
+
+    await name.type('RedBull Meeting' )
+
+
+    const capacity = await page.$('#capacity')
+
+    await capacity.type('90' )
+
+
+    const date = await page.$('#date')
+
+    await date.type('2023-08-15' )
+
+
+    const time = await page.$('#time')
+
+    await time.type('19:00' )
+
+   
+
+    const searchBtn = await page.$('#book-event-btn')
+
+    await searchBtn.click()
+
+   
+
+    const pageTitle = await page.title();
+
+   
+
+    assert(pageTitle === 'Report');
+
+    console.log("Title matched successfully");
+
+
+    await browser.close();
+
+    })();
+
 
 } catch (err) {
-    console.error(err);
-}
 
+    console.error(err);
+
+}
