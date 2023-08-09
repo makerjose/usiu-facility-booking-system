e9f9e7df9c224e7e59ef9196ccf4595798496205
nsa1im <100875129+nsa1im@users.noreply.github.com>
2023-08-09 09:30:30 -0700

    Fixed the navigation error

--- acceptance.js
+++ acceptance.js
@@ -5,7 +5,8 @@ try{
     const browser = await puppeteer.launch({headless:true});
     const page = await browser.newPage();
     await page.goto("https://usiu-facility-booking.onrender.com/");
-    await page.select('select#venue', 'auditorium'); 
+    const venue = await page.$('#venue');
+    await venue.select('auditorium'); 
     const name = await page.$('#name')
     await name.type('RedBull Meeting' )
     const capacity = await page.$('#capacity')
@@ -16,27 +17,11 @@ try{
     await time.type('19:00' )
     const bookBtn = await page.$('#book-event-btn')
     await bookBtn.click()
-    const pageTitle1 = await page.title();
-    await page.goto("https://usiu-facility-booking.onrender.com/event-report");
-    const bookNewBtn = await page.$('#book-new-event-btn')
-    await bookNewBtn.click()
-    const pageTitle2 = await page.title();
-    await page.goto("https://usiu-facility-booking.onrender.com/");
-    await page.select('select#venue', 'tvroom_2'); 
-    await name.type('Mercedes Meeting' )
-    await capacity.type('15' )
-    await date.type('2023-08-15' )
-    await time.type('20:00' )
-    await bookBtn.click()
-    await page.goto("https://usiu-facility-booking.onrender.com/event-report");
-    const viewEventsBtn = await page.$('#view-events-btn')
-    await viewEventsBtn.click()
-    const pageTitle3 = await page.title();
-    await page.goto("https://usiu-facility-booking.onrender.com/view-events?");
-    await bookNewBtn.click()
-    const pageTitle4 = await page.title();
-    assert(pageTitle1 === 'Report' && pageTitle2 === 'Home'  && pageTitle3 === 'Events' && pageTitle4 === 'Home');
-    console.log("Titles matched successfully");
+    const navigationPromise = page.waitForNavigation({ waitUntil: ['load', 'networkidle2'] });
+    await navigationPromise;
+    const pageTitle = await page.title();
+    assert(pageTitle === 'Report');
+    console.log("Title matched successfully");
     await browser.close();
     })();
 } catch (err) {
